52fc2867d4e48a1a816c500d4846dc1291a13d03
rgederin <gederin.ruslan@gmail.com>
2016-10-13 14:23:36 +0300

    third assignment initial commit

---
+++ src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,19 @@
+/**
+ * Created by rgederin on 10/13/2016.
+ */
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points){
+        // finds all line segments containing 4 points
+    }
+
+    public int numberOfSegments(){
+        // the number of line segments
+        return -1;
+    }
+
+
+    public LineSegment[] segments(){
+        // the line segments
+        return null;
+    }
+}
---
+++ src/main/java/FastCollinearPoints.java
@@ -0,0 +1,18 @@
+/**
+ * Created by rgederin on 10/13/2016.
+ */
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 or more points
+    }
+
+    public int numberOfSegments() {
+        // the number of line segments
+        return -1;
+    }
+
+    public LineSegment[] segments() {
+        // the line segments
+        return null;
+    }
+}
